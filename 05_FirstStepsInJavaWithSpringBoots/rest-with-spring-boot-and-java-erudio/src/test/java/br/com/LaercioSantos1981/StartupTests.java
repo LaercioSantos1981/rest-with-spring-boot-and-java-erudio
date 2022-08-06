b6e30da99be0b0b7f55557620f7f502bc66c61a1
Laercio Santos <laercioj_santos@hotmail.com>
2022-08-06 14:03:03 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoots/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/LaercioSantos1981/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.LaercioSantos1981;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
